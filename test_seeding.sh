7a4182a35f9e449e3ea5a011a64c51c59d3c9a7b
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2021-12-15 15:42:25 +0100

    Updated seeding test

--- test_seeding.sh
+++ test_seeding.sh
@@ -3,6 +3,8 @@
 export PYTHONHASHSEED=0
 export TF_NUM_INTEROP_THREADS=1
 export TF_NUM_INTRAOP_THREADS=1
+
+# when setting the following variable, code is not executed on GPU which provides deterministic behaviour
 # export CUDA_VISIBLE_DEVICES=-1
 
 /bin/python3 train.py --model VGG16 --lr 1e-2 --epochs 1 --n-batches 50 --batch-size 32 --gpu-monitor-interval -1 --seed 0
