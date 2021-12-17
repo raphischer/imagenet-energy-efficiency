14130de81d1c1a973eeb1ad760bbc7099a069942
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2021-12-17 09:56:11 +0100

    Adapted train commands

--- train_all.sh
+++ train_all.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
-# AlexNet and VGG
-for MODEL in AlexNet VGG11 VGG13 VGG16 VGG19
-do
-  python train.py --model $MODEL --lr 1e-2
-done
+### default parameters taken from https://github.com/pytorch/vision/tree/main/references/classification
 
-# ResNet
-for MODEL in ResNet18 ResNet34 ResNet50 ResNet101 ResNet152
-do
-  train.py --model $MODEL
-done
+export PYTHONHASHSEED=0
 
-# MobileNetV2
-train.py --model MobileNetV2 --epochs 300 --lr 0.045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.98
+python3 train.py --model VGG16 --lr 1e-2
+python3 train.py --model VGG19 --lr 1e-2
 
-# MobileNetV3 Large & Small
-for MODEL in MobileNetV3Large MobileNetV3Small
-do
-  train.py --model $MODEL --epochs 600 --opt rmsprop --batch-size 128 --lr 0.064\ 
-     --wd 0.00001 --lr-step-size 2 --lr-gamma 0.973 --auto-augment imagenet --random-erase 0.2
-done
+python3 train.py --model ResNet50
+python3 train.py --model ResNet101
+python3 train.py --model ResNet152 --batch-size 128 # 256 too much for A100
+
+python3 train.py --model MobileNetV2 --lr 0.045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.98
+python3 train.py --model MobileNetV3Small --opt rmsprop --lr 0.064 --wd 0.00001 --lr-step-size 2 --lr-gamma 0.973 --auto-augment imagenet --random-erase 0.2
+python3 train.py --model MobileNetV3Large --opt rmsprop --lr 0.064 --wd 0.00001 --lr-step-size 2 --lr-gamma 0.973 --auto-augment imagenet --random-erase 0.2
 
 # ResNext?
 
