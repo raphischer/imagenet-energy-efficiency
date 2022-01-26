c2044837886a0347b942c507eedb80af86fcaa1f
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2022-01-26 15:00:41 +0100

    Added scripts and removed legacy stuff

---
+++ scripts/train_50_epochs.sh
@@ -0,0 +1,12 @@
+# python3 train.py --model VGG16 --output-dir /raid/fischer/train50
+# python3 train.py --model VGG19 --output-dir /raid/fischer/train50
+# python3 train.py --model ResNet50 --output-dir /raid/fischer/train50
+# python3 train.py --model MobileNetV3Small --output-dir /raid/fischer/train50
+# python3 train.py --model QuickNetSmall --output-dir /raid/fischer/train50
+# python3 train.py --model MobileNetV2 --output-dir /raid/fischer/train50
+# python3 train.py --model QuickNet --output-dir /raid/fischer/train50
+python3 train.py --model EfficientNetB7 --output-dir /raid/fischer/train50
+python3 train.py --model DenseNet121 --output-dir /raid/fischer/train50
+python3 train.py --model NASNetLarge --output-dir /raid/fischer/train50
+python3 train.py --model InceptionV3 --output-dir /raid/fischer/train50
+python3 train.py --model ResNet152 --output-dir /raid/fischer/train50
