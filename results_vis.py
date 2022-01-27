08310e70ed7a4e864aa6be67faa9e86dc9b13115
Raphael Fischer <raphael.fischer@tu-dortmund.de>
2022-01-27 17:24:38 +0100

    Updated visualization

--- results_vis.py
+++ results_vis.py
@@ -10,7 +10,25 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-def create_fig(x, y, axis_title, names, spacing):
+def calc_accuracy(res, train=False, top5=False):
+    split = 'train' if train else 'validation'
+    metric = 'sparse_top_k_categorical_accuracy' if top5 else 'sparse_categorical_accuracy'
+    return res[split]['results']['metrics'][metric]
+
+
+def calc_parameters(res):
+    return res['train']['results']['model']['params']
+
+
+def calc_inf_time(res):
+    return res['train']['duration'] / 1281167 * 1000
+
+
+def calc_power_draw(res):
+    return res['train']["gpu_monitoring"]["total"]["total_power_draw"] / 1281167
+
+
+def create_scatter_fig(x, y, axis_title, names, spacing):
     fig = go.Figure()
     # areas
     spacing = [0] + list(np.cumsum(spacing))
@@ -55,12 +73,23 @@ class Visualization(dash.Dash):
 
     def __init__(self, results):
         super().__init__(__name__)
-        self.results = results
+        self.environments = results
         self.spacings = {
+            '5%, 15%, 35%, 50%': [0.05, 0.15, 0.35, 0.5],
             '10%, 20%, 30%, 40%': [0.1, 0.2, 0.3, 0.4],
             '25%, 25%, 25%, 25%': [0.25, 0.25, 0.25, 0.25],
-            '5%, 15%, 35%, 50%': [0.05, 0.15, 0.35, 0.5],
         }
+        self.axis_options = {
+            'Top-1 Validation Accuracy [%]': lambda res: calc_accuracy(res),
+            'Top-1 Training Accuracy [%]': lambda res: calc_accuracy(res, train=True),
+            'Top-5 Validation Accuracy [%]': lambda res: calc_accuracy(res, top5=True),
+            'Top-5 Training Accuracy [%]': lambda res: calc_accuracy(res, train=True, top5=True),
+            'Parameters': calc_parameters,
+            'Inference time / Sample [ms]': calc_inf_time,
+            'Power Draw / Sample [Ws]': calc_power_draw
+        }
+        self.xaxis_default = 'Top-1 Validation Accuracy [%]'
+        self.yaxis_default = 'Parameters'
         self.layout = html.Div(children=[
             dcc.Graph(
                 id='fig',
@@ -69,31 +98,72 @@ class Visualization(dash.Dash):
                 # config={'responsive': True},
                 # style={'height': '100%', 'width': '100%'}
             ),
-            dcc.Dropdown(
-                id='spacing',
-                options=[
-                    {'label': spac, 'value': spac} for spac in self.spacings.keys()
-                ],
-                value=list(self.spacings.keys())[0]
-            ),
+            html.Div(children=[
+                html.H2('Environment:'),
+                dcc.Dropdown(
+                    id='environment',
+                    options=[
+                        {'label': env, 'value': env} for env in self.environments.keys()
+                    ],
+                    value=list(self.environments.keys())[0]
+                ),
+            ]),
+            html.Div(children=[
+                html.H2('X-Axis:'),
+                dcc.Dropdown(
+                    id='xaxis',
+                    options=[
+                        {'label': env, 'value': env} for env in self.axis_options.keys()
+                    ],
+                    value=self.xaxis_default
+                ),
+            ]),
+            html.Div(children=[
+                html.H2('Y-Axis:'),
+                dcc.Dropdown(
+                    id='yaxis',
+                    options=[
+                        {'label': env, 'value': env} for env in self.axis_options.keys()
+                    ],
+                    value=self.yaxis_default
+                ),
+            ]),
+            html.Div(children=[
+                html.H2('Distribution:'),
+                dcc.Dropdown(
+                    id='spacing',
+                    options=[
+                        {'label': spac, 'value': spac} for spac in self.spacings.keys()
+                    ],
+                    value=list(self.spacings.keys())[0]
+                ),
+            ])
         ])
-        self.callback(Output('fig', 'figure'), Input('spacing', 'value')) (self.update_fig)
+        self.callback(Output('fig', 'figure'), [Input('environment', 'value'), Input('xaxis', 'value'), Input('yaxis', 'value'), Input('spacing', 'value')]) (self.update_fig)
 
-    def update_fig(self, spac=None):
+    def update_fig(self, env=None, xaxis=None, yaxis=None, spac=None):
+        if env is None:
+            results = list(self.environments.values())[0]
+        else:
+            results = self.environments[env]
         if spac is None:
             spacing = list(self.spacings.values())[0]
         else:
             spacing = self.spacings[spac]
-        results = self.results['TF_A100']
         names = list(results.keys())
-        accuracy = [res['validation']['results']['metrics']['accuracy'] for res in results.values()]
-        power_draw = [res['validation']["gpu_monitoring"]["total"]["total_power_draw"] / 3600 for res in results.values()]
-        return create_fig(accuracy, power_draw, ['Accuracy', 'Power Draw (Wh)'], names, spacing)
+        func_x = self.axis_options[self.xaxis_default] if xaxis is None else self.axis_options[xaxis]
+        func_y = self.axis_options[self.yaxis_default] if yaxis is None else self.axis_options[yaxis]
+        x = [func_x(res) for res in results.values()]
+        y = [func_y(res) for res in results.values()]
+        return create_scatter_fig(x, y, ['Accuracy', 'Power Draw (Wh)'], names, spacing)
 
 
 if __name__ == '__main__':
 
-    result_files = {'TF_A100': 'results_a100.json'}
+    result_files = {
+        'TF_A100': 'results_a100.json',
+        'TF_RTX5000': 'results_rtx5000.json'
+    }
     results = {}
     for name, resf in result_files.items():
         with open(resf, 'r') as r:
